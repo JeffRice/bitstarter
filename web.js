5f401225c3f9d292c94f9eb83769dde4a1dc5fe2
Jeffrey Rice <jrjeffreyrice@gmail.com>
2013-07-12 00:08:40 +0000

    trying sync call agai with request response from app

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
-var fs = require('fs');
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 var text = fs.readFileSync('index.html','utf8');
-console.log (text);
+console.log (text)
+
+app.get('/', function(request, response) {
+  response.send(text);
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
