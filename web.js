101365d13f0233c25ace206dd1ac97227e1a73ed
Jeffrey Rice <jrjeffreyrice@gmail.com>
2013-07-12 00:01:31 +0000

    trying sync call again2

--- web.js
+++ web.js
@@ -2,9 +2,8 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
-var text = fs.readFileSync('index.html','utf8')
-
-console.log (text)
+var text = fs.readFileSync('index.html','utf8');
+console.log (text);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
